fba4c3b0d4775eca398fed01db80b70a7cb63f55
PATEL <PATEL@LAPTOP-12VBQ1HB>
2023-03-01 22:50:16 +0530

    3rd test case uncommit

--- DeOn Project Automation Script/src/DeOnpackage/DeOnLogin.java
+++ DeOn Project Automation Script/src/DeOnpackage/DeOnLogin.java
@@ -29,31 +29,31 @@ public class DeOnLogin {
 	}
 	
  	
-//	@Test(priority=3)
-//	public void Users () throws InterruptedException, AWTException {
-//		Thread.sleep(2000);
-//		driver.findElement(By.linkText("Users")).click();
-//		Thread.sleep(2000);
-//		driver.findElement(By.cssSelector("button[type='button'][class='ant-btn ant-btn-primary userAddButton']")).click();
-//		Thread.sleep(2000);
-//		WebElement uploadButton = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/form/div[1]/div/div/span/div/div/span"));
-//		StringSelection q = new StringSelection("C:\\Users\\PATEL\\Downloads\\download (1).jpg");
-//		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(q,null);
-//
-//		Thread.sleep(2000);
-//		Robot r = new Robot();
-//		r.keyPress(KeyEvent.VK_CONTROL);
-//		r.keyPress(KeyEvent.VK_V);
-//		r.keyRelease(KeyEvent.VK_CONTROL);
-//		r.keyRelease(KeyEvent.VK_V);
-//      	r.keyPress(KeyEvent.VK_ENTER);
-//      	r.keyRelease(KeyEvent.VK_ENTER);
-//      	System.out.println("test");
-//		driver.findElement(By.id("brokerForm_first_name")).sendKeys("Jacob");
-//		driver.findElement(By.id("brokerForm_last_name")).sendKeys("aa");
-//		driver.findElement(By.id("brokerForm_email")).sendKeys("jacob_aa@mailinator.com");
-//		driver.findElement(By.id("brokerForm_phone")).sendKeys("5645644");
-//		driver.findElement(By.cssSelector("#brokerForm > div.ant-row.ant-row-end > button.ant-btn.ant-btn-submit")).click();
+	@Test(priority=3)
+	public void Users () throws InterruptedException, AWTException {
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Users")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("button[type='button'][class='ant-btn ant-btn-primary userAddButton']")).click();
+		Thread.sleep(2000);
+		WebElement uploadButton = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/form/div[1]/div/div/span/div/div/span"));
+		StringSelection q = new StringSelection("C:\\Users\\PATEL\\Downloads\\download (1).jpg");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(q,null);
+
+		Thread.sleep(2000);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		r.keyRelease(KeyEvent.VK_V);
+      	r.keyPress(KeyEvent.VK_ENTER);
+      	r.keyRelease(KeyEvent.VK_ENTER);
+      	System.out.println("test");
+		driver.findElement(By.id("brokerForm_first_name")).sendKeys("Jacob");
+		driver.findElement(By.id("brokerForm_last_name")).sendKeys("aa");
+		driver.findElement(By.id("brokerForm_email")).sendKeys("jacob_aa@mailinator.com");
+		driver.findElement(By.id("brokerForm_phone")).sendKeys("5645644");
+		driver.findElement(By.cssSelector("#brokerForm > div.ant-row.ant-row-end > button.ant-btn.ant-btn-submit")).click();
 	
 	
 //	@Test(priority=4)
