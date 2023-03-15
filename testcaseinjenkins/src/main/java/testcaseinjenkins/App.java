6cb712e8657c2a353b2a11218d9df06ee672eb6c
PATEL <PATEL@LAPTOP-12VBQ1HB>
2023-03-15 22:48:54 +0530

    open site

---
+++ testcaseinjenkins/src/main/java/testcaseinjenkins/App.java
@@ -0,0 +1,13 @@
+package testcaseinjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
@@ -0,0 +1,65 @@
+package testcase;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+	@Test
+	public class jenkinstest1 {
+			
+			WebDriver driver;
+			public void jenkinstest() throws InterruptedException, AWTException {
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Drivers\\chromedriver.exe");
+			
+		
+			
+			
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			WebDriver driver = new ChromeDriver(options);
+			driver.get("https://www.educastic.com/");
+			driver.manage().window().maximize();
+			Thread.sleep(2000);
+			driver.manage().window().fullscreen();
+			Thread.sleep(2000);
+			
+			Robot robot = new Robot();
+			Thread.sleep(2000);
+			robot.keyPress(KeyEvent.VK_ESCAPE);
+			Thread.sleep(2000);
+			robot.keyRelease(KeyEvent.VK_ESCAPE);
+			Dimension d = new Dimension(400,800);
+			driver.manage().window().setSize(d);
+			//driver.findElement(By.xpath("//*[@id=\"mySidenav2\"]/div/a[1]")
+			
+			
+			
+			
+//			WebDriver driver1;
+//			System.setProperty("webdriver.gecko.driver","D:\\Selenium\\s-lib\\geckodriver.exe");
+//			driver1 = new FirefoxDriver();
+//			driver1.get("https://www.educastic.com/");
+//			driver.manage().window().maximize();
+//			Thread.sleep(2000);
+//			driver.manage().window().fullscreen();
+//			Thread.sleep(2000);
+//			Robot robot1 = new Robot();
+//			Thread.sleep(4000);
+//			robot1.keyPress(KeyEvent.VK_ESCAPE);
+//			Thread.sleep(2000);
+//			robot1.keyRelease(KeyEvent.VK_ESCAPE);
+	//	
+		}
+
+	}
+	
+
