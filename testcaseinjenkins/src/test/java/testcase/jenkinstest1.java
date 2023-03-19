67cfad54299f9c23b790d329790f8a72d498bdcf
77Dhruvil <pateldhruvil2119976@gmail.com>
2023-03-19 22:42:19 +0530

    nayan bharada

--- testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
+++ testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
@@ -39,7 +39,7 @@ import org.testng.annotations.Test;
 			robot.keyRelease(KeyEvent.VK_ESCAPE);
 			Dimension d = new Dimension(400,800);
 			driver.manage().window().setSize(d);
-			System.out.println("Dhruvil is good boy always");
+			System.out.println("Nayan is a bludy fucking guy");
 			//driver.findElement(By.xpath("//*[@id=\"mySidenav2\"]/div/a[1]")
 			
 			
