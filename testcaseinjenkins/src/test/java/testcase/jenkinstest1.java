fba3dc580c9d905fecaf73dced5eb43c0aa335b0
77Dhruvil <pateldhruvil2119976@gmail.com>
2023-03-24 22:00:07 +0530

    learing123

--- testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
+++ testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
@@ -39,7 +39,7 @@ import org.testng.annotations.Test;
 			robot.keyRelease(KeyEvent.VK_ESCAPE);
 			Dimension d = new Dimension(400,800);
 			driver.manage().window().setSize(d);
-			System.out.println("jenkins 1 ");
+			System.out.println("jenkins 123");
 			//driver.findElement(By.xpath("//*[@id=\"mySidenav2\"]/div/a[1]")
 			
 			
