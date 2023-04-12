d771d823619ed6a62d1123f9e87b06d557492cd4
77Dhruvil <126700691+77Dhruvil@users.noreply.github.com>
2023-04-12 22:24:37 +0530

    Update jenkinstest1.java

--- testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
+++ testcaseinjenkins/src/test/java/testcase/jenkinstest1.java
@@ -39,7 +39,7 @@ import org.testng.annotations.Test;
 			robot.keyRelease(KeyEvent.VK_ESCAPE);
 			Dimension d = new Dimension(400,800);
 			driver.manage().window().setSize(d);
-			System.out.println("Dhruvil is back	again");
+			System.out.println("Dhruvil is back all");
 
 			//driver.findElement(By.xpath("//*[@id=\"mySidenav2\"]/div/a[1]")
 			
